374b938c93d99cd2ed8260a5623ed62b95f8319a
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-07 23:17:52 -0500

    replaced scroll event handlers with setIntervals, and now I have dangling intervals so this isn't really a good optimization job

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let navbar = document.getElementById('nav')
 let logo = document.getElementById('logo')
 let navGrid = document.getElementById('nav-grid')
 
+let shrinkAtTop = _ => {
+  navbar.style.height = '10%'
+  logo.style.height = '70%'
+  logo.style.left = '10px'
+  logo.style.top = '10px'
+  logo.style.filter = 'brightness(75%)';
+}
+
 let revertAtTop = _ => {
   if (window.pageYOffset === 0) {
     navbar.style.height = ''
@@ -12,12 +20,9 @@ let revertAtTop = _ => {
   }
 }
 
-document.addEventListener('scroll', _ => {
-  navbar.style.height = '10%'
-  logo.style.height = '70%'
-  logo.style.left = '10px'
-  logo.style.top = '10px'
-  logo.style.filter = 'brightness(75%)';
-}, { capture: false, passive: true});
-
-document.addEventListener('scroll', revertAtTop, {passive: true});
+setInterval(() => {
+  if (window.pageYOffset > 0)
+    shrinkAtTop()
+  if (window.pageYOffset === 0)
+    revertAtTop()
+}, 50);
