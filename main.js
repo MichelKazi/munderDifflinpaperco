f7e08f7214b4b8c2d19eda4378878186aab41b3c
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-07 21:21:09 -0500

    Now navbar turns into a top section when you scroll to top. Logo changes color to fit the background better. Colors are easier on the eyes, still boring and gray though.

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 let navbar = document.getElementById('nav')
 let logo = document.getElementById('logo')
 
+let revertAtTop = _ => {
+  if (window.pageYOffset === 0) {
+    navbar.style.height = ''
+    logo.style.height = ''
+    logo.style.left = ''
+    logo.style.top = ''
+    logo.style.filter = ''
+  }
+}
+
 document.addEventListener('scroll', _ => {
   navbar.style.height = '10%'
   logo.style.height = '70%'
   logo.style.left = '10px'
   logo.style.top = '10px'
+  logo.style.filter = 'brightness(75%)';
 });
+
+document.addEventListener('scroll', revertAtTop);
