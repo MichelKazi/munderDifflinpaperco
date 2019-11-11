9c80ff5ebcef52fbebedb3bda33e27e3af0e9510
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-10 22:09:54 -0500

    broke everything :)

--- main.js
+++ main.js
@@ -9,24 +9,24 @@ let intersect = document.getElementById('intersection-point')
 
 let shrinkAtTop = _ => {
   navbar.classList.replace('nav', 'nav-shrunk')
+  navbar.appendChild(logo)
   logo.classList.replace('logo', 'logo-shrunk')
   navGrid.classList.replace('nav-items', 'nav-items-shrunk')
   for (b of navbtn){
     b.classList.replace('nav-btn', 'nav-btn-shrunk')
   }
-  quote.style.left = '65.5%'
-  contactUs.style.left = '78.5%'
+
 }
 
 let revertAtTop = _ => {
   navbar.classList.replace('nav-shrunk', 'nav')
+  document.body.appendChild(logo)
   logo.classList.replace('logo-shrunk', 'logo')
   navGrid.classList.replace('nav-items-shrunk', 'nav-items')
   for (b of navbtn){
     b.classList.replace('nav-btn-shrunk', 'nav-btn')
   }
-  quote.style.left = '35.5%'
-  contactUs.style.left = '53.5%'
+
 }
 
 let resizer = new IntersectionObserver((entries) => {
@@ -39,11 +39,3 @@ let resizer = new IntersectionObserver((entries) => {
 })
 
 resizer.observe(intersect)
-
-//This is the bad thing to do
-// setInterval(() => {
-//   if (window.pageYOffset > 0)
-//     shrinkAtTop()
-//   if (window.pageYOffset === 0)
-//     revertAtTop()
-// }, 50);
