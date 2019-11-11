067ed4a488f540ab2a563a592e3dbff87f334e90
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-10 23:22:37 -0500

    oh man this looks infinitely better

--- main.js
+++ main.js
@@ -9,18 +9,13 @@ let shrinkAtTop = _ => {
   navbar.classList.replace('nav', 'nav-shrunk')
   logo.classList.replace('logo', 'logo-shrunk')
   navGrid.classList.replace('nav-grid', 'nav-grid-shrunk')
-  for (b of navbtn){
-    b.classList.replace('nav-btn', 'nav-btn-shrunk')
-  }
 }
+
 
 let revertAtTop = _ => {
   navbar.classList.replace('nav-shrunk', 'nav')
   logo.classList.replace('logo-shrunk', 'logo')
   navGrid.classList.replace('nav-grid-shrunk', 'nav-grid')
-  for (b of navbtn){
-    b.classList.replace('nav-btn-shrunk', 'nav-btn')
-  }
 }
 
 let resizer = new IntersectionObserver((entries) => {
