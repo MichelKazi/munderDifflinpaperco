171e0f092cb54c828dd6a5d72f9ef794d46bfd0b
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-10 19:20:04 -0500

    took buttons off grid

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 let navbar = document.getElementById('nav')
 let logo = document.getElementById('logo')
-let navGrid = document.getElementById('nav-grid')
+let navGrid = document.getElementById('nav-items')
 let navbtn = document.getElementsByClassName('btn')
+let quote = document.getElementById('quote')
+let contactUs = document.getElementById('contact-us')
 
 let intersect = document.getElementById('intersection-point')
 
 let shrinkAtTop = _ => {
   navbar.classList.replace('nav', 'nav-shrunk')
   logo.classList.replace('logo', 'logo-shrunk')
-  navGrid.classList.replace('nav-grid', 'nav-grid-shrunk')
+  navGrid.classList.replace('nav-items', 'nav-items-shrunk')
   for (b of navbtn){
     b.classList.replace('nav-btn', 'nav-btn-shrunk')
   }
+  quote.style.left = '65.5%'
+  contactUs.style.left = '78.5%'
 }
 
 let revertAtTop = _ => {
   navbar.classList.replace('nav-shrunk', 'nav')
   logo.classList.replace('logo-shrunk', 'logo')
-  navGrid.classList.replace('nav-grid-shrunk', 'nav-grid')
+  navGrid.classList.replace('nav-items-shrunk', 'nav-items')
   for (b of navbtn){
     b.classList.replace('nav-btn-shrunk', 'nav-btn')
   }
+  quote.style.left = '35.5%'
+  contactUs.style.left = '53.5%'
 }
 
 let resizer = new IntersectionObserver((entries) => {
@@ -41,14 +47,3 @@ resizer.observe(intersect)
 //   if (window.pageYOffset === 0)
 //     revertAtTop()
 // }, 50);
-
-for (b of navbtn) {
-  b.addEventListener('mouseover', _ => {
-    b.style.backgroundColor = '#faab00'
-    b.style.fontSize = '10px'
-  });
-  b.addEventListener('mouseout', _ => {
-    b.style.backgroundColor = ''
-    b.style.fontSize = ''
-  });
-}
